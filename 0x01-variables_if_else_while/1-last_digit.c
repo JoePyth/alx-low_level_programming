47f38b8cd9f15b7bb23f50bd378332ddf012b714
JoePython <mugaurijoe@gmail.com>
2023-02-18 12:59:35 -0800

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,24 +2,33 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(void) {
-    int n;
+/**
+ * main - main function to generate a random number
+ *
+ * Return: Always 0 (Success)
+ *
+ */
 
-    srand(time(0));
-    n = rand();
+int main(void)
+{
+	int n;
+	int num;
 
-    printf("Last digit of %d is ", n);
-
-    int last_digit = n % 10;
-
-    if (last_digit > 5) {
-        printf("%d and is greater than 5\n", last_digit);
-    } else if (last_digit == 0) {
-        printf("0 and is 0\n");
-    } else {
-        printf("%d and is less than 6 and not 0\n", last_digit);
-    }
-
-    return 0;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is ", n);
+	num = n % 10;
+	if (num > 5)
+	{
+		printf("%d and is greater than 5\n", num);
+	}
+	else if ((num < 6) && (num < 0))
+	{
+		printf("%d and is less than 6 and not 0\n", num);
+	}
+	else
+	{
+		printf("%d and is 0\n", num);
+	}
+	return (0);
 }
-
