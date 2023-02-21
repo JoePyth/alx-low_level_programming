740a75d57857b6737809aa9ca3bdbe541b5d79d3
JoePython <mugaurijoe@gmail.com>
2023-02-21 14:21:18 -0800

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,24 @@
-#include <unistd.h>
-#include <main.h>
+#include "main.h"
 
 /**
- * main - entry point
- *
- * Return - Always  0
-
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
-
-int main(void) 
+int main(void)
 {
-	int _putchar
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+	
 
 	
 
