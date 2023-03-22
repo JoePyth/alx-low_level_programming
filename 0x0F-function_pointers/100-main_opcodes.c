cc1fe8d5a1d5905ff9c216a6b6295a598b31102b
Joseph Mugauri <104701262+JoePyth@users.noreply.github.com>
2023-03-22 16:28:11 +0200

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
-	#include <stdlib.h>
+#include <stdlib.h>
 
+/**
+* main - prints its own opcodes
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: Always 0 (Success)
+*/
 
-	/**
-	 * main - prints its own opcodes
-	 * @argc: number of arguments
-	 * @argv: array of arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
-	int main(int argc, char *argv[])
+int main(int argc, char *argv[])
 	{
 		int bytes, i;
 		char *arr;
-
 
 		if (argc != 2)
 		{
 			printf("Error\n");
 			exit(1);
 		}
-
 
 		bytes = atoi(argv[1]);
 
@@ -31,9 +29,7 @@
 			exit(2);
 		}
 
-
 		arr = (char *)main;
-
 
 		for (i = 0; i < bytes; i++)
 		{
