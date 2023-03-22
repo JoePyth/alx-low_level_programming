e2e35bdfac5042054b20de296e10313a498bbdc2
Joseph Mugauri <104701262+JoePyth@users.noreply.github.com>
2023-03-22 20:35:48 +0200

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,11 +1,9 @@
 #include "3-calc.h"
-
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-
 /**
 * op_add - Returns the sum of two numbers.
 * @a: The first number.
@@ -13,7 +11,6 @@ int op_mod(int a, int b);
 *
 * Return: The sum of a and b.
 */
-
 int op_add(int a, int b)
 	{
 		return (a + b);
@@ -26,7 +23,6 @@ int op_add(int a, int b)
 *
 * Return: The difference of a and b.
 */
-
 int op_sub(int a, int b)
 	{
 		return (a - b);
@@ -38,7 +34,6 @@ int op_sub(int a, int b)
 *
 * Return: The product of a and b.
 */
-
 int op_mul(int a, int b)
 	{
 		return (a * b);
@@ -50,7 +45,6 @@ int op_mul(int a, int b)
 *
 * Return: The quotient of a and b.
 */
-
 int op_div(int a, int b)
 	{
 		return (a / b);
@@ -62,7 +56,6 @@ int op_div(int a, int b)
 *
 * Return: The remainder of the division of a by b.
 */
-
 int op_mod(int a, int b)
 	{
 		return (a % b);
