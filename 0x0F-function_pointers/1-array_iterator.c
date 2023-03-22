e4205dc2a58a986caec648dfedbd2227c574b2e0
Joseph Mugauri <104701262+JoePyth@users.noreply.github.com>
2023-03-22 20:29:27 +0200

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,5 @@
 #include "function_pointers.h"
 #include <stdio.h>
-
 /**
 * array_iterator - prints each array elem on a newl
 * @array: array
@@ -8,7 +7,6 @@
 * @action: pointer to print in regular or hex
 * Return: void
 */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 	
 	{
