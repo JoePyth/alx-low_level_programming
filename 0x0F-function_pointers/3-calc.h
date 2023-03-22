a2a134993404b393f5e7b1199587bed9d52b9d31
Joseph Mugauri <104701262+JoePyth@users.noreply.github.com>
2023-03-22 16:29:06 +0200

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,16 +1,17 @@
 #ifndef CALC_H
-	#define CALC_H
-	/**
-	 * struct op - A struct op.
-	 * @op: The operator.
-	 * @f: The associated function.
-	 */
-	typedef struct op
+#define CALC_H
+
+/**
+* struct op - A struct op.
+* @op: The operator.
+* @f: The associated function.
+*/
+
+typedef struct op
 	{
 		char *op;
 		int (*f)(int a, int b);
 	} op_t;
-
 
 	int op_add(int a, int b);
 	int op_sub(int a, int b);
@@ -18,7 +19,6 @@
 	int op_div(int a, int b);
 	int op_mod(int a, int b);
 	int (*get_op_func(char *s))(int, int);
-
 
 	#endif
 
